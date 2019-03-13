38b31d475f98a09523898ea3f2cec49c4b649db9
Fabrizio <webmaster.developer@outlook.com>
2019-03-13 15:53:14 -0400

    building palindrome

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #define SIZE 40
 #define PALINDROME_DEFAULT "IRONX"
 
 const char *progname;
 void usage();
+char **new_palindrome(char *name, unsigned int length);
+void print_palindrome(char **palindrome, unsigned int length);
 
 int main(int argc, char* argv[]) {
-    unsigned int i=0, length; char *name;
+    unsigned int i=0, length; char *name; char c;
     progname = argv[0];
     if (argc < 2) {
         name = PALINDROME_DEFAULT;
@@ -20,18 +23,40 @@ int main(int argc, char* argv[]) {
             return 0;
         }
         while( name[i] ) {
-            const char c = toupper(name[i]);
+            c = toupper(name[i]);
             if (c < 'A' || 'Z' < c) {
-                printf("Character not valid: %c\n", c);
+                printf("Character not valid: %c\n\n", c);
                 return 1;
             }
             name[i] = c; i++;
         }
     }
     length = (unsigned int) strlen(name);
-    printf("Palindrome of \"%s\" (length=%u):\n\n", name, length);
+    char **palindrome = new_palindrome(name, length);
+    printf("\nPalindrome of \"%s\":\n\n", name);
+    print_palindrome(palindrome, length);
+    free(palindrome);   // Clear memory
 }
 
 void usage() {
     printf("Usage: %s <string>\n\n", progname);
+}
+
+char **new_palindrome(char *name, unsigned int length) {
+    unsigned int i;
+    // Allocate space for n string pointers (n=length)
+    char **palindrome_array = (char **) malloc(sizeof(char*)*length);
+    palindrome_array[0] = name;
+    palindrome_array[length-1] = name;
+    for (i=0; i<length; i++) {
+
+    }
+    return palindrome_array;
+}
+
+void print_palindrome(char **palindrome, unsigned int length) {
+    unsigned int i;
+    for (i=0; i<length; i++) {
+        printf("%s\n", palindrome[i]);
+    }
 }
